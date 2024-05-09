febb14fa7f8d0f3a705139520174812f34e79296
Kimpulla <haukka.kimi@gmail.com>
2024-05-09 20:51:02 +0300

    After killing npc say something in chat.

--- src/main/java/com/guiltTripper/GuiltTripperPlugin.java
+++ src/main/java/com/guiltTripper/GuiltTripperPlugin.java
@@ -6,7 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
+import net.runelite.api.NPC;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.NpcDespawned;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
@@ -14,10 +16,6 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
 import javax.sound.sampled.FloatControl;
-
-import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.AudioInputStream;
@@ -25,6 +23,11 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 
 @Slf4j
 @PluginDescriptor(
@@ -41,17 +44,15 @@ public class GuiltTripperPlugin extends Plugin {
 	private GuiltTripperConfig config;
 
 	private final String[] messages = GuiltTripperMessages.MESSAGES;
-
 	private final Random random = new Random();
 	private Timer timer;
+	private Clip clip = null;
 
 	@Override
 	protected void startUp() throws Exception {
-		//System.out.println("Guilt Tripper started!");
 		startTimer();
 	}
 
-	// Starts timer after startup.
 	private void startTimer() {
 		if (timer != null) {
 			timer.cancel();
@@ -69,12 +70,11 @@ public class GuiltTripperPlugin extends Plugin {
 					});
 				}
 			}
-		}, 15 * 60 * 1000, 15 * 60 * 1000); // 15 min
+		}, 15 * 60 * 1000, 15 * 60 * 1000);
 	}
 
 	@Override
 	protected void shutDown() throws Exception {
-		System.out.println("Guilt Tripper stopped!");
 		if (timer != null) {
 			timer.cancel();
 		}
@@ -88,38 +88,48 @@ public class GuiltTripperPlugin extends Plugin {
 		}
 	}
 
-	// Prints random message.
+	@Subscribe
+	public void onNpcDespawned(NpcDespawned event) {
+		NPC npc = event.getNpc();
+		if (BossName.NPC_NAMES.contains(npc.getName()) && npc.getHealthRatio() == 0) {
+			printSpecificMessage("You have a funny feeling like you're being followed.");
+
+			// Create a new timer to handle delayed message display
+			new Timer().schedule(new TimerTask() {
+				@Override
+				public void run() {
+					clientThread.invokeLater(() -> {
+						printSpecificMessage("Turns out it was just a lost kitten.");
+					});
+				}
+			}, 5000);  // Delay of 5000 milliseconds (5 seconds)
+		}
+	}
+
+
 	private void printRandomMessage() {
 		String randomMessage = "<col=ff0000>" + messages[random.nextInt(messages.length)] + "</col>";
 		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", randomMessage, null);
 	}
 
-	// Prints specific message.
 	private void printSpecificMessage(String message) {
 		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "<col=ff0000>" + message + "</col>", null);
 	}
 
-	// By default = null.
-	private Clip clip = null;
-
-	// Plays sound.
 	private void playSound() {
-		if (!config.allowSound()) {  // Check, if sound is allowed.
+		if (!config.allowSound()) {
 			return;
 		}
 
 		if (clip != null) {
 			clip.close();
-			clip = null;  // Set Clip to null after closing it.
+			clip = null;
 		}
 
-		// List of sound effects.
 		String[] sounds = {"laughter.wav", "ba-dum-tishh.wav", "oldman.wav", "ooouuh.wav", "sneeze.wav", "trumpet.wav"};
-		Random random = new Random();
-		int index = random.nextInt(sounds.length);  // Random index.
+		int index = random.nextInt(sounds.length);
 
 		try {
-			// Download random sound effect.
 			URL soundUrl = getClass().getResource("/" + sounds[index]);
 			if (soundUrl == null) {
 				System.out.println("Sound file not found!");
@@ -128,7 +138,7 @@ public class GuiltTripperPlugin extends Plugin {
 			try (AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundUrl)) {
 				clip = AudioSystem.getClip();
 				clip.open(audioIn);
-				setVolume(clip, config.soundVolume()); // Use users set volume.
+				setVolume(clip, config.soundVolume());
 				clip.start();
 			}
 		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
@@ -136,22 +146,16 @@ public class GuiltTripperPlugin extends Plugin {
 		}
 	}
 
-
-	// Volume modification.
 	private void setVolume(Clip clip, int volume) {
 		if (volume < 0 || volume > 100) {
 			throw new IllegalArgumentException("Volume must be between 0 and 100");
 		}
 
 		FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-		float minDb = gainControl.getMinimum(); // esim. -80 dB tai vastaava
-		float maxDb = gainControl.getMaximum(); // +6.0206 dB
-
-		// dB value linearly.
+		float minDb = gainControl.getMinimum();
+		float maxDb = gainControl.getMaximum();
 		float gain = ((maxDb - minDb) * (volume / 100.0f)) + minDb;
 		gainControl.setValue(gain);
-
-		System.out.println("Volume set to " + volume + "% (" + gain + " dB)");
 	}
 
 	@Provides
