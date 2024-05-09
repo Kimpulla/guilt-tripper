de65b726880b2c90f5b6e24a6300ff49479da0ba
Kimpulla <haukka.kimi@gmail.com>
2024-05-09 20:50:17 +0300

    Created boss names list.

---
+++ src/main/java/com/guiltTripper/BossName.java
@@ -0,0 +1,54 @@
+package com.guiltTripper;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class BossName {
+    public static final Set<String> NPC_NAMES = new HashSet<>();
+
+    static {
+        NPC_NAMES.add("Abyssal Sire");
+        NPC_NAMES.add("Alchemical Hydra");
+        NPC_NAMES.add("Artio");
+        NPC_NAMES.add("Callisto");
+        NPC_NAMES.add("Cerberus");
+        NPC_NAMES.add("Chaos Elemental");
+        NPC_NAMES.add("Chaos Fanatic");
+        NPC_NAMES.add("Commander Zilyana");
+        NPC_NAMES.add("Corporeal Beast");
+        NPC_NAMES.add("Dagannoth Kings");
+        NPC_NAMES.add("Dagannoth Prime");
+        NPC_NAMES.add("Dagannoth Rex");
+        NPC_NAMES.add("Dagannoth Supreme");
+        NPC_NAMES.add("Duke Sucellus");
+        NPC_NAMES.add("General Graardor");
+        NPC_NAMES.add("Giant Mole");
+        NPC_NAMES.add("Goblin");
+        NPC_NAMES.add("Grotesque Guardians");
+        NPC_NAMES.add("Kalphite Queen");
+        NPC_NAMES.add("King Black Dragon");
+        NPC_NAMES.add("Kree'arra");
+        NPC_NAMES.add("Kraken");
+        NPC_NAMES.add("K'ril Tsutsaroth");
+        NPC_NAMES.add("Nex");
+        NPC_NAMES.add("Phantom Muspah");
+        NPC_NAMES.add("Phosani's Nightmare");
+        NPC_NAMES.add("Sarachnis");
+        NPC_NAMES.add("Scorpia");
+        NPC_NAMES.add("Scurrius");
+        NPC_NAMES.add("Skotizo");
+        NPC_NAMES.add("Spindel");
+        NPC_NAMES.add("Thermonuclear Smoke Devil");
+        NPC_NAMES.add("The Leviathan");
+        NPC_NAMES.add("The Nightmare");
+        NPC_NAMES.add("The Whisperer");
+        NPC_NAMES.add("TzKal-Zuk");
+        NPC_NAMES.add("TzTok-Jad");
+        NPC_NAMES.add("Vardorvis");
+        NPC_NAMES.add("Venenatis");
+        NPC_NAMES.add("Vet'ion");
+        NPC_NAMES.add("Vorkath");
+        NPC_NAMES.add("Zalcano");
+        NPC_NAMES.add("Zulrah");
+    }
+}
