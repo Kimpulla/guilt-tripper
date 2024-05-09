64080d750ff0859d028d999f9b6a708769fcf7c5
Kimpulla <haukka.kimi@gmail.com>
2024-05-09 15:44:00 +0300

    Created messages file and added more messages.

---
+++ src/main/java/com/guiltTripper/GuiltTripperMessages.java
@@ -0,0 +1,25 @@
+package com.guiltTripper;
+
+public class GuiltTripperMessages {
+    public static final String[] MESSAGES = {
+            "Touch grass!",
+            "Go level up in daylight skills!",
+            "Have you tried the breathing skill outside?",
+            "Your chair called. It wants a relationship break.",
+            "Maybe it’s time to grind some real-life quests?",
+            "Even a goblin has better social skills!",
+            "I bet even a rock crab walks more than you.",
+            "Time to logout and log into reality!",
+            "Have you considered leveling up offline?",
+            "Get some sunlight, you're not a cave slime!",
+            "Even your pet's on a more balanced diet. Time for a walk?",
+            "When was the last time you wrote something by hand?",
+            "Is this truly an adventure, or just an escape?",
+            "Your character's alignment: Chaotic Screen-staring.",
+            "If AFK were a skill tree, you'd be maxed out.",
+            "In it for the achievements, or just to rage-quit?",
+            "Enjoying the grind or just grinding your teeth?",
+            "Achievement hunter or just hunting for the exit?",
+            "Have you heard about hypeks? Me neither."
+    };
+}
