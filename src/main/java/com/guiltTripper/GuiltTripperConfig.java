07fa944a52b78300970b7e5d9f122559b0314549
Kimpulla <haukka.kimi@gmail.com>
2024-05-15 17:42:07 +0300

    Changed config group.

--- src/main/java/com/guiltTripper/GuiltTripperConfig.java
+++ src/main/java/com/guiltTripper/GuiltTripperConfig.java
@@ -5,7 +5,7 @@ import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 import net.runelite.client.config.Range;
 
-@ConfigGroup("example")
+@ConfigGroup("guiltTripper")
 public interface GuiltTripperConfig extends Config {
 
 	@ConfigItem(
